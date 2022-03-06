b50ed8108b2fd56cb2adb26e253a524974ff914f
Nbiba Bedis <bedisnbiba@gmail.com>
2022-03-06 09:20:19 +0100

    Add search

--- islands/StatioMain.tsx
+++ islands/StatioMain.tsx
@@ -90,6 +90,66 @@ const Stations = (
   );
 };
 
+function SearchStations() {
+  const [input, setInput] = useState("");
+  const [method, setMethod] = useState("Country");
+  const [stations, setStations] = useState<StationType[]>([]);
+
+  // deno-lint-ignore no-explicit-any
+  const handleChange = (e: any) => {
+    setInput(e.target.value);
+  };
+  // deno-lint-ignore no-explicit-any
+  const handleMethod = (e: any) => {
+    setMethod(e.target.value);
+  };
+
+  async function search() {
+    let urlMethod: string;
+    switch (method) {
+      case "Country":
+        urlMethod = "bycountry";
+        break;
+      case "Language":
+        urlMethod = "bylanguage";
+        break;
+      case "Name":
+        urlMethod = "byname";
+        break;
+      default:
+        throw "ureachable";
+    }
+    setStations(
+      await (
+        await fetch(
+          `https://de1.api.radio-browser.info/json/stations/${urlMethod}/${input}`,
+          {
+            "headers": {
+              "User-Agent": "https://github.com/sigmaSd/freshRadio",
+            },
+          },
+        )
+      ).json(),
+    );
+  }
+
+  return (
+    <div>
+      <h2>Search Stations</h2>
+      <input onChange={handleChange} value={input} />
+      <select onChange={handleMethod}>
+        <option>Country</option>
+        <option>Language</option>
+        <option>Name</option>
+      </select>
+      <button onClick={search}>Search</button>
+      {stations.length !== 0 && (
+        <Stations title="Search Results" stations={stations} />
+      )}
+    </div>
+  );
+}
+
 export default function StationMain() {
   const [stations, setStations] = useState<StationType[]>([]);
   useEffect(() => {
@@ -101,6 +161,7 @@ export default function StationMain() {
     <div>
       {stations !== null &&
         <Stations title="Local Stations" stations={stations} />}
+      <SearchStations />
     </div>
   );
 }
