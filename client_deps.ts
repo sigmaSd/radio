ba28a97b268ec9968c2e7a288ebe5d026c2b4d6d
Nbiba Bedis <bedisnbiba@gmail.com>
2022-04-26 21:34:59 +0100

    use a fixed commit of fresh for future proffing

--- client_deps.ts
+++ client_deps.ts
@@ -1,1 +1,3 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
+// export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
+// use a known working commit
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/59e6398d7724f4990aa3ace12dce36e11b20e1ba/runtime.ts";
--- server_deps.ts
+++ server_deps.ts
@@ -1,1 +1,3 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.ts";
+//export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.ts";
+// use a known working commit
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/59e6398d7724f4990aa3ace12dce36e11b20e1ba/server.ts";
