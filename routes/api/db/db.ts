dc6c135aacda5676cd1454ed6ebb21691afe01d5
Nbiba Bedis <bedisnbiba@gmail.com>
2022-07-03 09:26:58 +0100

    faster db query

--- routes/api/db/db.ts
+++ routes/api/db/db.ts
@@ -17,12 +17,23 @@ const jsonDb: StationDBType[] = JSON.parse(
   await Deno.readTextFile("static/db/compressed_db.json"),
 );
 
-for (const station of jsonDb) {
-  try {
-    db.query(
-      `insert into radio_table values ('${station.name}', '${station.country}', '${station.language}', ${station.votes}, '${station.url}', '${station.favicon}')`,
-    );
-  } catch {
-    // some rows are broken, ignore
-  }
-}
+const sanitize = (s: string) => s.replaceAll('"', "").replaceAll("'", "");
+db.query(
+  `insert into radio_table values ${
+    jsonDb.map((station) =>
+      "('" +
+      sanitize(station.name) +
+      "','" +
+      sanitize(station.country) +
+      "','" +
+      sanitize(station.language) +
+      "'," +
+      station.votes +
+      ",'" +
+      sanitize(station.url) +
+      "','" +
+      sanitize(station.favicon) +
+      "')"
+    ).join(",")
+  }`,
+);
