c12ee198e35d6d8f7e3e6a32a1ca88d66905a86d
Nbiba Bedis <bedisnbiba@gmail.com>
2022-07-23 09:19:16 +0100

    Handle url encoded countries

--- routes/api/db/bycountry/[country].ts
+++ routes/api/db/bycountry/[country].ts
@@ -4,7 +4,7 @@ import { db } from "../db.ts";
 
 export const handler: Handlers = {
   GET(_req, ctx) {
-    const { country } = ctx.params;
+    const country = decodeURIComponent(ctx.params.country); // handle things like "United%20Kingdom"
 
     const countries = db.queryEntries(
       `SELECT * FROM radio_table WHERE Country LIKE '%${country}%'`,
