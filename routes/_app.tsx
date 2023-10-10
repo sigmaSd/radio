fac9d6aba76b746ecf4b0a2b6577aed645020742
Nbiba Bedis <bedisnbiba@gmail.com>
2023-10-10 20:43:13 +0100

    partial

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,19 +1,22 @@
 import { Head } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/server.ts";
 import NavigationBar from "@/components/NavigationBar.tsx";
+import { Partial } from "$fresh/runtime.ts";
 
 export default function App({ Component }: AppProps) {
   return (
     <div>
-      <body class="m-0">
-      </body>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavigationBar />
-      <div class="text-center p-4 mx-auto">
-        <Component />
-      </div>
+      <body f-client-nav>
+        <Partial name="body">
+          <div class="text-center p-4 mx-auto">
+            <Component />
+          </div>
+        </Partial>
+      </body>
     </div>
   );
 }
