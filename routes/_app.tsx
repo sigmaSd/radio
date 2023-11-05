033dfe4a63e69a4b788a44aeb9873f9aa9d1b3b1
Nbiba Bedis <bedisnbiba@gmail.com>
2023-11-05 16:53:39 +0100

    use new svg favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component }: AppProps) {
     <>
       <Head>
         <title>Rodio</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <NavigationBar />
       <body f-client-nav>
