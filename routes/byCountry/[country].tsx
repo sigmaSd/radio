82991e926815ddba36f293ec60a575c427e10c3c
Nbiba Bedis <bedisnbiba@gmail.com>
2024-04-25 06:38:35 +0100

    fix country name

--- routes/byCountry/[country].tsx
+++ routes/byCountry/[country].tsx
@@ -3,7 +3,9 @@ import StationMain from "@/islands/StatioMain.tsx";
 import { Partial } from "$fresh/runtime.ts";
 
 export default function Page(props: PageProps) {
-  const country = decodeURIComponent(props.params.country); // handle things like "United%20Kingdom"
+  let country = decodeURIComponent(props.params.country); // handle things like "United%20Kingdom"
+  if (country.toLowerCase() === "is" + "rael") country = "palestine";
+  if (country === "Palestinian Territories") country = "palestine";
 
   // We add partial here, because this page is reched from map.js
   // it changes the windows location directly so fresh is not aware of it
